b7bcf2af87eeb07d1cd66a0c606bfbdcc1e71969
Richard Chalk <barmybrit@gmail.com>
2023-11-10 22:03:49 +0100

    Version 2

--- LibraryAndServices/Program.cs
+++ LibraryAndServices/Program.cs
@@ -6,10 +6,20 @@ namespace LibraryAndServices
     {
         static void Main(string[] args)
         {
-            var firstCustomer = new 
-                Customer("Richard", "rich@email.com");
-            Console.WriteLine(firstCustomer.Name);
-            Console.WriteLine(firstCustomer.Email);
+            var customers = new List<Customer>
+            {
+                new Customer("Richard", "rich@email.com"),
+                new Customer("Stefan", "stefan@email.com"),
+                new Customer("Linda", "linda@email.com"),
+                new Customer("Alicia", "alicia@email.com"),
+
+            };
+
+            foreach (var c in customers)
+            {
+                Console.WriteLine($"Name: {c.Name}, Email: {c.Email}");
+            }
+
         }
     }
 }
