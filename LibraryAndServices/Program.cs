bd7298b16622202d7322a0fdc6e14e1988acdfe8
Richard Chalk <barmybrit@gmail.com>
2023-11-10 07:06:55 +0100

    Add project files.

---
+++ LibraryAndServices/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyClassLibrary;
+
+namespace LibraryAndServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstCustomer = new 
+                Customer("Richard", "rich@email.com");
+            Console.WriteLine(firstCustomer.Name);
+            Console.WriteLine(firstCustomer.Email);
+        }
+    }
+}
---
+++ MyClassLibrary/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace MyClassLibrary
+{
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string? Address { get; set; }
+        public int? PostNumber { get; set; }
+        public string? City { get; set; }
+        public bool? IsVIP { get; set; }
+
+        public Customer(string name, string email)
+        {
+            CustomerId = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+    }
+}
